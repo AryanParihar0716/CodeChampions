5878b441e630f927e654fffdadcc30307975008d
Neevs1 <neevan.redkar@gmail.com>
2026-03-26 17:30:33 +0530

    Improved visa logic

--- server/main.py
+++ server/main.py
@@ -12,11 +12,34 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 # Simple Agentic Logic
 def visa_check_node(state: dict):
     dest = state.get("destination", "").lower()
     # Mock Logic: Indian passport context
-    visa = "REQUIRED (Hard Process) ⚠️" if "usa" in dest or "uk" in dest else "Visa on Arrival / E-Visa ✅"
+    e_visa = ['Argentina', 'Armenia', 'Azerbaijan', 'Bahrain', 'Benin', 'Colombia', 'Cote D\'Ivoire', 'Djibouti', 'Georgia', 'Kazakhstan', 'Kyrgyzstan Republic', 'Lesotho', 'Malaysia', 'Moldova', 'New Zealand', 'Oman', 'Papua New Guinea', 'Russian Federation', 'Singapore', 'South Korea', 'Taiwan', 'Turkey', 'Uganda', 'Uzbekistan', 'Zambia']
+    visa_free = ['Barbados', 'Bhutan', 'Dominica', 'Grenada', 'Haiti', 'Hong Kong', 'Maldives', 'Mauritius', 'Montserrat', 'Nepal', 'Niue Island', 'Saint Vincent & the Grenadines', 'Samoa', 'Senegal', 'Serbia', 'Trinidad & Tobago']
+    Visa_on_arrival = [
+    'Angola', 'Bolivia', 'Cabo Verde', 'Cameroon', 'Cook Islands', 'Fiji', 'Guinea Bissau',
+    'Indonesia', 'Iran', 'Jamaica', 'Jordan', 'Kiribati', 'Laos', 'Madagascar', 'Mauritania',
+    'Nigeria', 'Qatar', 'Republic of Marshall Islands', 'Reunion Island', 'Rwanda', 'Seychelles',
+    'Somalia', 'Tunisia', 'Tuvalu', 'Vanuatu', 'Zimbabwe'
+    ]
+
+    VoA_Evisa = [
+    'Kenya', 'Myanmar', 'Saint Lucia', 'Sri Lanka', 'Suriname', 'Tajikistan', 'Tanzania',
+    'Thailand', 'Vietnam', 'Ethiopia', 'Cambodia'
+    ]
+    if dest in e_visa:
+        visa = "e-Visa"
+    elif dest in visa_free:
+        visa = "Visa-Free"
+    elif dest in Visa_on_arrival:
+        visa = "Visa on Arrival"
+    elif dest in VoA_Evisa:
+        visa = "Visa on Arrival or e-Visa"
+    else:
+        visa = "Visa Required"
     return {"visa_status": visa}
 
 workflow = StateGraph(dict)
